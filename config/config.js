1be20027dc12a6118ab07c6ec57d4b70383f815c
kitce <kitce.dev@gmail.com>
2018-07-06 00:21:43 +0800

    Create records directory

--- config/config.js
+++ config/config.js
@@ -1,28 +1,31 @@
 const Joi = require('joi');
+const mkdirp = require('mkdirp');
 const path = require('path');
 
 require('dotenv').config();
 
 const envVarsSchema = Joi.object({
-  NODE_ENV : Joi.string()
-                .allow(['development', 'production', 'test'])
-                .default('development'),
-  SHOW_NIGHTMARE : Joi.boolean().default(false),
-  USER_AGENT : Joi.string().default('Mozilla/5.0')
+  NODE_ENV: Joi.string()
+    .allow(['development', 'production', 'test'])
+    .default('development'),
+  SHOW_NIGHTMARE: Joi.boolean().default(false),
+  USER_AGENT: Joi.string().default('Mozilla/5.0')
 }).unknown().required();
 
-const {error, value : envVars} = Joi.validate(process.env, envVarsSchema);
+const {error, value: envVars} = Joi.validate(process.env, envVarsSchema);
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
 const config = {
-  env : envVars.NODE_ENV,
-  showNightmare : envVars.SHOW_NIGHTMARE,
-  userAgent : envVars.USER_AGENT,
-  dateFormat : 'YYYY-MM-DD',
-  recordsDirectory : path.resolve('data/records'),
-  noDrawDatesFilepath : path.resolve('data/no-draw-dates.json')
+  env: envVars.NODE_ENV,
+  showNightmare: envVars.SHOW_NIGHTMARE,
+  userAgent: envVars.USER_AGENT,
+  dateFormat: 'YYYY-MM-DD',
+  recordsDirectory: path.resolve('data/records'),
+  noDrawDatesFilepath: path.resolve('data/no-draw-dates.json')
 };
+
+mkdirp.sync(config.recordsDirectory);
 
 module.exports = config;
